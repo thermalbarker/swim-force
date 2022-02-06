f1b27d75bc837931e6460add516db0a5ab46903b
Thomas Barber <thermalbarker@gmail.com>
2022-02-06 15:08:21 +0100

    First prototype of swim force measurement

---
+++ SwimForce.ino
@@ -0,0 +1,107 @@
+#include "HX711.h"
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+
+#define DOUT  3
+#define CLK  2
+byte statLED = 13; //On board status LED
+
+HX711 scale;
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+const int buttonPin = 4;
+
+float calibration_factor = 1585; // Should be Newtons
+
+float distance = 0.0;
+
+long lastTime = 0;
+long lastMeasure = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Swim Force");
+  scale.begin(DOUT, CLK);
+  scale.set_scale(calibration_factor);
+  scale.tare(); //Reset the scale to 0
+
+  matrix.begin(0x70);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+
+  // read the state of the pushbutton value:
+  int buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    distance = 0;
+    scale.tare(); //Reset the scale to 0
+  }
+
+  long deltaT = millis() - lastMeasure;
+  if (deltaT > 100) {
+    lastMeasure = millis();
+    float force = scale.get_units();
+
+    // Force = 0.5 * rho * v^2 * C_D * A
+    // v = K * sqrt(F)
+    // K = sqrt(2/(rho * C_D * A))
+    // rho = 997 kg/m^3 (Density of water)
+    // C_D = between 0.5 - 1.0 (Drag constant)
+    // A = 0.1 m^2 ?? (Cross section)
+    // K = 0.1416 (Precompute constant K)
+    // 
+    // According to https://ftvs.cuni.cz/FTVS-2339-version1-the_determination_of_drag_in_front_crawl.pdf
+    // F = k * v ** n
+    // Empirically measure 1.5 < n < 2.5 and 16 < k < 25
+    // and K = 1 / sqrt(k)
+    // Assuming n = 2 to simplify calculations, then
+    // 0.2 < K < 0.25 
+    float velocity = 0.0;
+    // Only measure if the force is positive
+    // to avoid negative velocities
+    if (force > 0.0) {
+        velocity = 0.1416 * sqrt(force);
+    }
+    float deltaD = velocity * deltaT * 1e-3;
+    distance += deltaD;
+
+    float power = velocity * force;
+
+    matrix.print(distance);
+    matrix.writeDisplay();
+
+    Serial.print("Force: ");
+    Serial.print(force, 1);
+    Serial.print(" Velocity: ");
+    Serial.print(velocity, 1);
+    Serial.print(" DeltaT: ");
+    Serial.print(deltaT, 1);
+    Serial.print(" DeltaD: ");
+    Serial.print(deltaD, 1);
+    Serial.print(" distance: ");
+    Serial.print(distance, 1);
+    Serial.println();
+  }
+
+  //Each second blink the status LED
+  if (millis() - lastTime > 1000)
+  {
+    lastTime = millis();
+    if (digitalRead(LED_BUILTIN) == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  
+  }
+}
